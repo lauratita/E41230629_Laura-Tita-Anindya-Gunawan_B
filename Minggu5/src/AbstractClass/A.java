b7c96440d54f775d71885163a6f9eee5157e619e
Laura Tita <lauratita316@gmail.com>
2024-02-27 21:44:11 +0700

    abstract class

---
+++ Minggu5/src/AbstractClass/A.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AbstractClass;
+
+/**
+ *
+ * @author Neendy's
+ */
+
+//deklarasi class
+public abstract class A {
+//    deklarasi method abstrak
+    abstract public void method2();
+//    method konkrit yang memiliki implementasi
+    public void method1(){
+        System.out.println("Method konkrit dari class A");
+    }
+}
---
+++ Minggu5/src/AbstractClass/B.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AbstractClass;
+
+/**
+ *
+ * @author Neendy's
+ */
+public class B extends A {
+//    method abstrak di-override dan dibuat
+    public void method2(){
+        System.out.println("Method abstract yang sudah menjadi konkrit dalam class B");
+    }
+}
